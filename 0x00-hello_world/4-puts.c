346d9a2fda61aef37ad628622d788fd5eb126716
Zubbyhub <nzubechukwuwisdom794@gmail.com>
2023-02-16 16:39:51 -0800

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+1 #include <stdio.h>
+2 /**
+3 *  * main - Entry point,
+4 *   * Print a line of code using puts
+5 *    * ends with a new line
+6 *     *
+7 *      * Return: Always 0 (Success)
+8 *       */
+9 int main(void)
+10 {
+11 		puts("\"Programming is like building a multilingual puzzle");
+12 			return (0);
+13 }
